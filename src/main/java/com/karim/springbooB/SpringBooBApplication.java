7d492bf532a7bbc28ae6bd20087ec6df84f1b798
BAGGARI <karim.baggari@capgemini.com>
2023-09-06 02:14:55 +0100

    Initial commit

---
+++ src/main/java/com/karim/springbooB/SpringBooBApplication.java
@@ -0,0 +1,13 @@
+package com.karim.springbooB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBooBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBooBApplication.class, args);
+		System.out.println("Hello World");
+	}
+}
---
+++ src/test/java/com/karim/springbooB/SpringBooBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karim.springbooB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBooBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
