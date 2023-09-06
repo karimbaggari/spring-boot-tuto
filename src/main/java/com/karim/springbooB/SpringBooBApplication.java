ed7a9dc4cc0af81eaa5d04b5d8886e4ede266c3d
BAGGARI <karim.baggari@capgemini.com>
2023-09-07 00:28:38 +0100

    create the first request method with hello world message

--- src/main/java/com/karim/springbooB/SpringBooBApplication.java
+++ src/main/java/com/karim/springbooB/SpringBooBApplication.java
@@ -2,6 +2,9 @@ package com.karim.springbooB;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class SpringBooBApplication {
@@ -9,5 +12,29 @@ public class SpringBooBApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBooBApplication.class, args);
 		System.out.println("Hello World");
+	}
+	@RestController
+	class Resource {
+		@RequestMapping(
+				method = RequestMethod.GET
+		)
+		Message getMessage() {
+			return new Message("Hello world");
+		}
+	}
+	class Message {
+		private final String message;
+
+		Message(String message) {
+			this.message = message;
+		}
+
+		public String getMessage() {
+			return message;
+		}
+		@Override
+		public  String toString() {
+			return "Message{" + "message='" + message + "\'" + '}';
+		}
 	}
 }
