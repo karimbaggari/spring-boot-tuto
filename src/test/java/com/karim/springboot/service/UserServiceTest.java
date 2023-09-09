3db27424f68b191fcd5335e72d76d81974d86480
BAGGARI <karim.baggari@capgemini.com>
2023-09-09 05:05:42 +0100

    add shouldUpdateUser test for UserService updateUser method

--- src/test/java/com/karim/springboot/service/UserServiceTest.java
+++ src/test/java/com/karim/springboot/service/UserServiceTest.java
@@ -5,15 +5,18 @@ import com.karim.springboot.DAO.FakeDataDao;
 import com.karim.springboot.model.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import static org.assertj.core.api.Java6Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 
 class UserServiceTest {
     @Mock
@@ -39,19 +42,36 @@ class UserServiceTest {
         assertThat(allUsers).hasSize(1);
         User user = allUsers.get(0);
 
-        assertThat(user.getFirstName()).isEqualTo("rosa");
-        assertThat(user.getLastName()).isEqualTo("montana");
-        assertThat(user.getGender()).isEqualTo(User.Gender.FEMALE);
-        assertThat(user.getAge()).isEqualTo(22);
-        assertThat(user.getEmail()).isEqualTo("rosa.montana@gmail.com");
+        assertUserFields(user);
+    }
+
+
+
+    @Test
+    void shouldGetUser() throws Exception {
+        UUID annaUid = UUID.randomUUID();
+        User annaUser = new User(annaUid,"anna","montana", User.Gender.FEMALE,22,"anna.montana@gmail.com");
+        given(fakeDataDao.selectUserByUserUid(annaUid)).willReturn(Optional.of(annaUser));
+        Optional<User> userOptional = userService.getUser(annaUid);
+        assertThat(userOptional.isPresent()).isTrue();
+        User user = userOptional.get();
+        assertUserFields(user);
     }
 
     @Test
-    void getUser() {
-    }
+    void shouldUpdateUser() throws Exception {
+        UUID annaUid = UUID.randomUUID();
+        User annaUser = new User(annaUid,"anna","montana", User.Gender.FEMALE,22,"anna.montana@gmail.com");
+        given(fakeDataDao.selectUserByUserUid(annaUid)).willReturn(Optional.of(annaUser));
+        given(fakeDataDao.updateUser(annaUser)).willReturn(1);
 
-    @Test
-    void updateUser() {
+        ArgumentCaptor<User> captor = ArgumentCaptor.forClass(User.class);
+        int resultUpdateService = userService.updateUser(annaUser);
+        verify(fakeDataDao).selectUserByUserUid(annaUid);
+        verify(fakeDataDao).updateUser(captor.capture());
+        User user = captor.getValue();
+        assertUserFields(user);
+        assertThat(resultUpdateService).isEqualTo(1);
     }
 
     @Test
@@ -60,5 +80,12 @@ class UserServiceTest {
 
     @Test
     void insertUser() {
+    }
+    private static void assertUserFields(User user) {
+        assertThat(user.getFirstName()).isEqualTo("anna");
+        assertThat(user.getLastName()).isEqualTo("montana");
+        assertThat(user.getGender()).isEqualTo(User.Gender.FEMALE);
+        assertThat(user.getAge()).isEqualTo(22);
+        assertThat(user.getEmail()).isEqualTo("anna.montana@gmail.com");
     }
 }
