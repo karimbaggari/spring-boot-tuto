cf95d66d806521bae02c68e41893e459df691215
BAGGARI <karim.baggari@capgemini.com>
2023-09-09 04:41:35 +0100

    add first test Service for getAllUsers

--- src/test/java/com/karim/springboot/service/UserServiceTest.java
+++ src/test/java/com/karim/springboot/service/UserServiceTest.java
@@ -1,5 +1,6 @@
 package com.karim.springboot.service;
 
+import com.google.inject.internal.util.ImmutableList;
 import com.karim.springboot.DAO.FakeDataDao;
 import com.karim.springboot.model.User;
 import org.junit.jupiter.api.BeforeEach;
@@ -8,9 +9,11 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import java.util.List;
+import java.util.UUID;
 
 import static org.assertj.core.api.Java6Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
 
 class UserServiceTest {
     @Mock
@@ -28,8 +31,19 @@ class UserServiceTest {
 
     @Test
     void shouldGetAllUsers() throws Exception {
+        UUID rosaUserUid = UUID.randomUUID();
+        User rosaUser = new User(rosaUserUid,"rosa","montana", User.Gender.FEMALE,22,"rosa.montana@gmail.com");
+        ImmutableList<User> users = new ImmutableList.Builder<User>().add(rosaUser).build();
+        given(fakeDataDao.selectAllUsers()).willReturn(users);
         List<User> allUsers = userService.getAllUsers();
         assertThat(allUsers).hasSize(1);
+        User user = allUsers.get(0);
+
+        assertThat(user.getFirstName()).isEqualTo("rosa");
+        assertThat(user.getLastName()).isEqualTo("montana");
+        assertThat(user.getGender()).isEqualTo(User.Gender.FEMALE);
+        assertThat(user.getAge()).isEqualTo(22);
+        assertThat(user.getEmail()).isEqualTo("rosa.montana@gmail.com");
     }
 
     @Test
